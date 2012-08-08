7391128045ea8ebdf0a3de2a1beaadadff1dfa89
Daniel MacDougall <dmacdougall@gmail.com>
2012-08-07 18:27:48 -0700

    Update pinion configuration to work with 0.2.2

--- app.rb
+++ app.rb
@@ -2,20 +2,30 @@ require "bundler/setup"
 require "pathological"
 require "sinatra/base"
 require "sinatra/reloader"
+require "pinion"
+require "pinion/sinatra_helpers"
+require "bourbon"
 require "coffee-script"
 require "sinatra/content_for2"
 
 class App < Sinatra::Base
-  helpers Sinatra::ContentFor2
+  include Pinion::SinatraHelpers
+
+  set :pinion, Pinion::Server.new("/assets")
+
+  configure do
+    pinion.convert :sass => :css
+    pinion.convert :coffee => :js
+  end
 
   configure :development do
     register Sinatra::Reloader
   end
 
-  def initialize(pinion)
-    @pinion = pinion
-    super
-  end
+  pinion.watch "public"
+  pinion.watch "#{Gem.loaded_specs["bourbon"].full_gem_path}/app/assets/stylesheets"
+
+  helpers Sinatra::ContentFor2
 
   get "/" do
     erb :index
--- config.ru
+++ config.ru
@@ -1,13 +1,9 @@
 require "./app"
-require "pinion"
-require "bourbon"
 
-PINION_MOUNT_POINT = "/assets"
-pinion = Pinion::Server.new(PINION_MOUNT_POINT)
-pinion.convert :sass => :css
-pinion.convert :coffee => :js
-pinion.watch "public"
-pinion.watch "#{Gem.loaded_specs["bourbon"].full_gem_path}/app/assets/stylesheets"
+map App.pinion.mount_point do
+  run App.pinion
+end
 
-map(PINION_MOUNT_POINT) { run pinion }
-map("/") { run App.new(pinion) }
+map "/"  do
+  run App
+end
